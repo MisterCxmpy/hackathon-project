28ad0eb3393e99dcc12d15c8abde410b00fcc285
lewis <lewis.takavarasha@gmail.com>
2023-03-03 11:49:30 +0000

    condense choice function

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 
-function textBasedAdventure() {
+const textBasedAdventure = (count,choice)=> {
     h1.textContent = start.description
     h2.textContent = start.choiceA
     h3.textContent = start.choiceB
-    return choice1 = AorB
+    return choice = choice.command
 }
 
-function textBasedAdventure2(choice1) {
-    h1.textContent = start.(choice1).description
-    h2.textContent = start.(choice1).choiceA
-    h3.textContent = start.(choice1).choiceB
-    return choice2 = choice1.choice2
-}
+// const textBasedAdventure2 = (choice1)=> {
+//     h1.textContent = start.(choice1).description
+//     h2.textContent = start.(choice1).choiceA
+//     h3.textContent = start.(choice1).choiceB
+//     return choice2 = choice1.choice2
+// }
 
-function textBasedAdventure3(choice2) {
-    h1.textContent = start.(choice2).description
-    h2.textContent = start.(choice2).choiceA
-    h3.textContent = start.(choice2).choiceB
-    return choice3 = choice2.choice3
-}
+// const textBasedAdventure3 = (choice2)=> {
+//     h1.textContent = start.(choice2).description
+//     h2.textContent = start.(choice2).choiceA
+//     h3.textContent = start.(choice2).choiceB
+//     return choice3 = choice2.choice3
+// }
 
-function textBasedAdventureEnd(choice3) {
-    h1.textContent = start.(choice3).description
-}
+// const textBasedAdventureEnd = (choice3)=> {
+//     h1.textContent = start.(choice3).description
+// }
